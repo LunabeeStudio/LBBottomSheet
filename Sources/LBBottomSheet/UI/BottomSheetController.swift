1ba9801b25d518fad9f287ae164a453dfda0791c
Nicolas Dominati <nicolas.dominati@lunabee.com>
2021-11-08 15:48:46 +0100

    Improvement on minHeight swipe down management when can't dismiss.

--- Sources/LBBottomSheet/UI/BottomSheetController.swift
+++ Sources/LBBottomSheet/UI/BottomSheetController.swift
@@ -335,7 +335,7 @@ private extension BottomSheetController {
             newBottom = 0.0
         } else if destinationHeight < minHeight {
             newHeight = minHeight
-            newBottom = destinationHeight - minHeight
+            newBottom = behavior.canBeDismissed ? destinationHeight - minHeight : behavior.elasticityFunction(destinationHeight - minHeight)
         } else {
             newHeight = destinationHeight
             newBottom = 0.0
