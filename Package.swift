a9fdf5f2f95326dd6577f57f4e5275d7836b8c27
Nicolas Dominati <nicolas.dominati@lunabee.com>
2025-12-22 10:37:27 +0100

    Package config update.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LBBottomSheet",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "LBBottomSheet",
@@ -16,5 +16,5 @@ let package = Package(
             name: "LBBottomSheet",
             dependencies: [])
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageModes: [.v5]
 )
